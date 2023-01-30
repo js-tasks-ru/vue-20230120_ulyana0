716ba1484155f08ee0b4c1b8995dcae481a64e1d
LAPTOP-MGCESL29\Ekz <ekz.lin@gmail.com>
2023-01-30 18:38:11 +1000

    Решение VueCalculator

--- 01-basics/02-VueCalculator/script.js
+++ 01-basics/02-VueCalculator/script.js
@@ -1,3 +1,26 @@
-// import { createApp } from './vendor/vue.esm-browser.js';
+import { createApp, defineComponent } from './vendor/vue.esm-browser.js';
 
 // Создайте Vue приложение
+const App = defineComponent({
+  name: 'App',
+  data() {
+    return {
+      num1: 0,
+      num2: 0,
+      operator: ""
+    };        
+  }, 
+  computed: {
+    total() {
+      switch(this.operator) {
+        case "sum": return this.num1 + this.num2
+        case "subtract": return this.num1 - this.num2
+        case "multiply": return this.num1 * this.num2
+        case "divide": return this.num1 / this.num2
+        default: return 0
+      }
+    }, 
+  } 
+});    
+ const app = createApp(App);
+ const vm = app.mount('#app');
